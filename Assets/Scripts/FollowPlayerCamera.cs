bd823b8fa7171aeb424bba03e8213b3a91805569
Roosevelt <treyboyland@aol.com>
2019-01-26 19:06:16 -0600

    Completed the majority of the city. A few coins are missing.

--- Assets/Scripts/FollowPlayerCamera.cs
+++ Assets/Scripts/FollowPlayerCamera.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class FollowPlayerCamera : MonoBehaviour
 {
+    [SerializeField]
+    Vector2 offset;
+
     void Update()
     {
         Vector3 pos = gameObject.transform.position;
         Vector3 playerPos = PlayerController.Player.transform.position;
-        pos.x = playerPos.x;
-        pos.y = playerPos.y;
+        pos.x = playerPos.x + offset.x;
+        pos.y = playerPos.y + offset.y;
 
         gameObject.transform.position = pos;
     }
